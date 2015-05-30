aeb4078dd984a31a33a30434009df0050146308e
Natasha <pesci.nt@gmail.com>
2015-05-31 00:38:34 +0300

    pdfFile generate and save information

--- MDI_Project/Purchase_SaleForm.cs
+++ MDI_Project/Purchase_SaleForm.cs
@@ -14,6 +14,10 @@ namespace MDI_Project
     public partial class Purchase_SaleForm : Form
     {
         SQLiteConnection sql = new SQLiteConnection(@"Data Source=base.sqlite;Version=3");
+        public string name = "";
+        public string amount = "";
+        public string measure = "";
+        public string date = "";
 
         public Purchase_SaleForm()
         {
@@ -43,38 +47,56 @@ namespace MDI_Project
             sql.Open();
             string check_name = "SELECT Name FROM Goods  WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
             SQLiteCommand check = new SQLiteCommand(check_name, sql);
-            string name = check.ExecuteNonQuery().ToString();
-            string amount = Convert.ToString(nUDamount.Value);
-            string measure = Convert.ToString(cMBmeasure.SelectedItem);
-            string date = Convert.ToString(nUDday.Value) + "." + Convert.ToString(nUDamount.Value) + "." + Convert.ToString(nUDyear.Value);
+            name = check.ExecuteNonQuery().ToString();
+            amount = Convert.ToString(nUDamount.Value);
+            measure = Convert.ToString(cMBmeasure.SelectedItem);
+            date = Convert.ToString(nUDday.Value) + "." + Convert.ToString(nUDamount.Value) + "." + Convert.ToString(nUDyear.Value);
 
-
-
-
-
-
-
-
-
-
-
+            string c = "";
             string get_amount = "SELECT Amount FROM Goods WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
             SQLiteCommand get = new SQLiteCommand(get_amount, sql);
-            
-            int q = get.ExecuteNonQuery();
-            MessageBox.Show(q.ToString());
-            //SQLiteCommand get = new SQLiteCommand(get_amount, sql);
-            //string q = Convert.ToString(get.ExecuteNonQuery());    
-            //int difference = get.ExecuteNonQuery() - Convert.ToInt32(nUDamount.Value);
-
-            string update_goods_amount = "UPDATE Goods SET Amount = " + Convert.ToString(get.ExecuteNonQuery() - nUDamount.Value)  + " WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
-            //SQLiteCommand update = new SQLiteCommand(update_goods_amount, sql);
-            //update.ExecuteNonQuery();
+            get.ExecuteNonQuery();
+            SQLiteDataReader reader = get.ExecuteReader();
+            while (reader.Read())
+            {
+                c = reader[0] + "";
+            }
+            int difference = Convert.ToInt32(c) - Convert.ToInt32(amount);
+            string update_goods_amount = "UPDATE Goods SET Amount = " + (Convert.ToInt32(c) - Convert.ToInt32(amount)) + " WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
+            SQLiteCommand update = new SQLiteCommand(update_goods_amount, sql);
+            update.ExecuteNonQuery();
             MessageBox.Show("Операция выполнена успешно");
+
+
+
+            string q = "";
+            string get_min_amount = "SELECT MinAmount FROM Goods WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
+            SQLiteCommand get_min = new SQLiteCommand(get_min_amount, sql);
+            get_min.ExecuteNonQuery();
+            SQLiteDataReader reader_min = get_min.ExecuteReader();
+            while (reader_min.Read())
+            {
+                q = reader_min[0] + "";
+            }
+
+            if (Convert.ToInt32(c) < Convert.ToInt32(q))
+                try
+                {
+                    Check_Amount();
+                }
+                catch
+                {
+
+                }
+            Check_Amount();
             sql.Close();
         }
 
+        public void Check_Amount()
+        {
 
+            pdf.Add_PDF(name, amount, measure, date);
+        }
 
     }
 }
