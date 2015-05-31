ff5022a2ca9c00538b9991d8d75dcb21055c0d3e
Natasha <pesci.nt@gmail.com>
2015-05-31 23:31:46 +0300

    sending mail

--- MDI_Project/AMTgoods.cs
+++ MDI_Project/AMTgoods.cs
@@ -14,130 +14,145 @@ namespace MDI_Project
 {
     class AMTgoods
     {
-            public Document _doc;
-            private AMTgoods()
+        public Document _doc;
+        public void Add_PDF(string id, string name, string amount, string measure, string date, string price, string final_price)
+        {
+            string id_add = id;
+            string name_add = name;
+            string amount_add = amount;
+            string measure_add = measure;
+            string date_add = date;
+            string price_add = price;
+            string final_price_add = final_price;
+
+            _doc = new Document();
+            string path = @"C:\Temp\PdfFile.pdf";
+            try
             {
-                _doc = new Document();
-                string path = @"C:\Temp\PdfFile.pdf";
-                try
-                {
-                    File.Delete(path);
-                }
-                catch { }
-                PdfWriter.GetInstance(_doc, new FileStream(path, FileMode.CreateNew));
-                _doc.Open();
-
-                CreateTable();
-
-                _doc.Close();
+                File.Delete(path);
             }
+            catch { }
+            PdfWriter.GetInstance(_doc, new FileStream(path, FileMode.CreateNew));
+            _doc.Open();
+            CreateTable(id_add, name_add, amount_add, measure_add, date_add, price_add, final_price_add);
+            _doc.Close();
+        }
 
-            public static void GenerateFile()
-            {
-                var file = new AMTgoods();
-            }
+        public static void GenerateFile()
+        {
+            var file = new AMTgoods();
+        }
 
-            private void CreateTable()
-            {
-                string fg = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "Fradm.TTF");
-                BaseFont fgBaseFont = BaseFont.CreateFont(fg, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-                Font fgFont = new Font(fgBaseFont, 9, Font.NORMAL, BaseColor.BLACK);
+        private void CreateTable(string id_add, string name_add, string measure_add, string amount_add, string date_add, string price_add, string final_price_add)
+        {
+            string fg = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "Fradm.TTF");
+            BaseFont fgBaseFont = BaseFont.CreateFont(fg, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+            Font fgFont = new Font(fgBaseFont, 9, Font.NORMAL, BaseColor.BLACK);
 
-                var title = new Paragraph("Счет на оплату", fgFont);
-                title.Alignment = Element.ALIGN_CENTER;
-                title.SpacingAfter = 5;
+            var title = new Paragraph("Счет на оплату", fgFont);
+            title.Alignment = Element.ALIGN_CENTER;
+            title.SpacingAfter = 5;
 
-                var chapter = new Chapter(1);
-                var table = new PdfPTable(6);
-                Phrase p;
+            var chapter = new Chapter(1);
+            var table = new PdfPTable(7);
+            Phrase p;
 
-                p = new Phrase("номер по порядку", fgFont);
-                table.AddCell(CreateCell(p, rows: 2));
+            p = new Phrase("номер по порядку", fgFont);
+            table.AddCell(CreateCell(p, rows: 2));
 
-                p = new Phrase("товар", fgFont);
-                table.AddCell(CreateCell(p, rows: 1));
+            p = new Phrase("товар", fgFont);
+            table.AddCell(CreateCell(p, rows: 1));
 
-                p = new Phrase("единица измерения", fgFont);
-                table.AddCell(CreateCell(p, rows: 1));
+            p = new Phrase("единица измерения", fgFont);
+            table.AddCell(CreateCell(p, rows: 1));
 
-                p = new Phrase("количество", fgFont);
-                table.AddCell(CreateCell(p, rows: 2));
+            p = new Phrase("количество", fgFont);
+            table.AddCell(CreateCell(p, rows: 2));
 
-                p = new Phrase("цена за единицу товара", fgFont);
-                table.AddCell(CreateCell(p, rows: 2));
+            p = new Phrase("цена за единицу товара", fgFont);
+            table.AddCell(CreateCell(p, rows: 2));
 
-                p = new Phrase("общая цена", fgFont);
-                table.AddCell(CreateCell(p, rows: 2));
+            p = new Phrase("общая цена", fgFont);
+            table.AddCell(CreateCell(p, rows: 2));
 
-                p = new Phrase("наименование", fgFont);
-                table.AddCell(CreateCell(p));
+            p = new Phrase("дата", fgFont);
+            table.AddCell(CreateCell(p, rows: 2));
 
-                p = new Phrase("наименование", fgFont);
-                table.AddCell(CreateCell(p));
+            p = new Phrase("наименование", fgFont);
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase("1 ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase("наименование", fgFont);
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase("2 ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase("1 ");
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase("3 ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase("2 ");
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase("4 ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase("3 ");
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase("5 ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase("4 ");
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase("6 ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase("5 ");
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase(" ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase("6 ");
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase(" ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase("7 ");
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase(" ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase(id_add);
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase(" ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase(name_add);
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase(" ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase(amount_add);
+            table.AddCell(CreateCell(p));
 
-                p = new Phrase(" ");
-                table.AddCell(CreateCell(p));
+            p = new Phrase(measure_add);
+            table.AddCell(CreateCell(p));
 
-                table.SpacingAfter = 10;
+            p = new Phrase(price_add);
+            table.AddCell(CreateCell(p));
 
-                var subtitle1 = new Paragraph("___________", fgFont);
-                subtitle1.Alignment = Element.ALIGN_RIGHT;
+            p = new Phrase(final_price_add);
+            table.AddCell(CreateCell(p));
 
-                var subtitle = new Paragraph("Подпись", fgFont);
-                subtitle.Alignment = Element.ALIGN_RIGHT;
-                subtitle.SpacingAfter = 5;
+            p = new Phrase(date_add);
+            table.AddCell(CreateCell(p));
 
-                chapter.Add(title);
-                chapter.Add(table);
-                chapter.Add(subtitle1);
-                chapter.Add(subtitle);
-                _doc.Add(chapter);
-            }
+            table.SpacingAfter = 10;
 
-            private PdfPCell CreateCell(Phrase phrase, int rows = 1, int cols = 1)
-            {
-                var cell = new PdfPCell(phrase);
-                cell.Colspan = cols;
-                cell.Rowspan = rows;
+            var subtitle1 = new Paragraph("___________", fgFont);
+            subtitle1.Alignment = Element.ALIGN_RIGHT;
 
-                return cell;
-            }
+            var subtitle = new Paragraph("Подпись", fgFont);
+            subtitle.Alignment = Element.ALIGN_RIGHT;
+            subtitle.SpacingAfter = 5;
 
-            private void sample() 
-            { 
+            chapter.Add(title);
+            chapter.Add(table);
+            chapter.Add(subtitle1);
+            chapter.Add(subtitle);
+            _doc.Add(chapter);
+        }
+
+        public PdfPCell CreateCell(Phrase phrase, int rows = 1, int cols = 1)
+        {
+            var cell = new PdfPCell(phrase);
+            cell.Colspan = cols;
+            cell.Rowspan = rows;
+
+            return cell;
+        }
+
+        public void sample()
+        {
             SQLiteConnection sql = new SQLiteConnection(@"Data Source=base.sqlite;Version=3");
             sql.Open();
             string selectCmnd = "SELECT Name From Goods";
@@ -146,11 +161,8 @@ namespace MDI_Project
 
             SQLiteCommand sqlcmndN = new SQLiteCommand(selectCmnd, sql);
             SQLiteCommand sqlcmndP = new SQLiteCommand(selectCmnd1, sql);
-            SQLiteCommand sqlcmndA= new SQLiteCommand(selectCmnd2, sql);
+            SQLiteCommand sqlcmndA = new SQLiteCommand(selectCmnd2, sql);
             sql.Close();
-            }
-            
-
-            
         }
+    }
 }
--- MDI_Project/Purchase_SaleForm.cs
+++ MDI_Project/Purchase_SaleForm.cs
@@ -14,16 +14,20 @@ namespace MDI_Project
     public partial class Purchase_SaleForm : Form
     {
         SQLiteConnection sql = new SQLiteConnection(@"Data Source=base.sqlite;Version=3");
+        public string id = "";
         public string name = "";
         public string amount = "";
         public string measure = "";
         public string date = "";
+        public string price = "";
+        public string final_price = "";
+
+        AMTgoods pdf = new AMTgoods();
 
         public Purchase_SaleForm()
         {
             InitializeComponent();
             AddToComboBox();
-
         }
 
         private void AddToComboBox()
@@ -42,60 +46,148 @@ namespace MDI_Project
             sql.Close();
         }
 
+
         private void button2_Click(object sender, EventArgs e)
         {
-            sql.Open();
-            string check_name = "SELECT Name FROM Goods  WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
-            SQLiteCommand check = new SQLiteCommand(check_name, sql);
-            name = check.ExecuteNonQuery().ToString();
-            amount = Convert.ToString(nUDamount.Value);
-            measure = Convert.ToString(cMBmeasure.SelectedItem);
-            date = Convert.ToString(nUDday.Value) + "." + Convert.ToString(nUDamount.Value) + "." + Convert.ToString(nUDyear.Value);
-
-            string c = "";
-            string get_amount = "SELECT Amount FROM Goods WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
-            SQLiteCommand get = new SQLiteCommand(get_amount, sql);
-            get.ExecuteNonQuery();
-            SQLiteDataReader reader = get.ExecuteReader();
-            while (reader.Read())
+            if ((cMBgoods.SelectedIndex != -1) && (cMBmeasure.SelectedIndex != -1) && (nUDamount.Value != 0))
             {
-                c = reader[0] + "";
+                //Получение данных для построения заказа
+                sql.Open();
+
+                string current_name = "";
+                string check_name = "SELECT Name FROM Goods  WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
+                SQLiteCommand get_name = new SQLiteCommand(check_name, sql);
+                SQLiteDataReader reader_name = get_name.ExecuteReader();
+                while (reader_name.Read())
+                {
+                    current_name = reader_name[0] + "";
+                }
+
+                string current_price = "";
+                string check_price = "SELECT Price FROM Goods  WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
+                SQLiteCommand get_price = new SQLiteCommand(check_price, sql);
+                SQLiteDataReader reader_price = get_price.ExecuteReader();
+                while (reader_price.Read())
+                {
+                    current_price = reader_price[0] + "";
+                }
+
+                string current_id = "";
+                string check_id = "SELECT GoodsID FROM Goods  WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
+                SQLiteCommand get_id = new SQLiteCommand(check_id, sql);
+                SQLiteDataReader reader_id = get_id.ExecuteReader();
+                while (reader_id.Read())
+                {
+                    current_id = reader_id[0] + "";
+                }
+
+                id = current_id;
+                name = current_name;
+                amount = Convert.ToString(nUDamount.Value);
+                measure = Convert.ToString(cMBmeasure.SelectedItem);
+                date = Convert.ToString(nUDday.Value) + "." + Convert.ToString(nUDamount.Value) + "." + Convert.ToString(nUDyear.Value);
+                price = current_price;
+                final_price = (Convert.ToInt32(current_price) * Convert.ToInt32(nUDamount.Value)).ToString();
+                //Нахождение разницы того сколько товаров было и того сколько купили
+
+                string c = "";
+                string get_amount = "SELECT Amount FROM Goods WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
+                SQLiteCommand get = new SQLiteCommand(get_amount, sql);
+                get.ExecuteNonQuery();
+                SQLiteDataReader reader = get.ExecuteReader();
+                while (reader.Read())
+                {
+                    c = reader[0] + "";
+                }
+
+                int difference = Convert.ToInt32(c) - Convert.ToInt32(amount);
+                string update_goods_amount = "UPDATE Goods SET Amount = " + (Convert.ToInt32(c) - Convert.ToInt32(amount)) + " WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
+                SQLiteCommand update = new SQLiteCommand(update_goods_amount, sql);
+                update.ExecuteNonQuery();
+                MessageBox.Show("Операция выполнена успешно");
+
+                //проверка количества товаров на складе после оформления операции купли-продажи
+                string q = "";
+                string get_min_amount = "SELECT MinAmount FROM Goods WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
+                SQLiteCommand get_min = new SQLiteCommand(get_min_amount, sql);
+                get_min.ExecuteNonQuery();
+                SQLiteDataReader reader_min = get_min.ExecuteReader();
+                while (reader_min.Read())
+                {
+                    q = reader_min[0] + "";
+                }
+                if (Convert.ToInt32(c) < Convert.ToInt32(q))
+                    try
+                    {
+                        Check_Amount();
+                    }
+                    catch
+                    {
+                        MessageBox.Show(" Невозможно отослать запрос на покупку товара. Проверьте , заполнена ли быза данных поставщиков ");
+                    }
+                sql.Close();
             }
-            int difference = Convert.ToInt32(c) - Convert.ToInt32(amount);
-            string update_goods_amount = "UPDATE Goods SET Amount = " + (Convert.ToInt32(c) - Convert.ToInt32(amount)) + " WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
-            SQLiteCommand update = new SQLiteCommand(update_goods_amount, sql);
-            update.ExecuteNonQuery();
-            MessageBox.Show("Операция выполнена успешно");
-
-
-
-            string q = "";
-            string get_min_amount = "SELECT MinAmount FROM Goods WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
-            SQLiteCommand get_min = new SQLiteCommand(get_min_amount, sql);
-            get_min.ExecuteNonQuery();
-            SQLiteDataReader reader_min = get_min.ExecuteReader();
-            while (reader_min.Read())
+            else
             {
-                q = reader_min[0] + "";
+                MessageBox.Show(" Не все поля заполнены ");
             }
 
-            if (Convert.ToInt32(c) < Convert.ToInt32(q))
-                try
-                {
-                    Check_Amount();
-                }
-                catch
-                {
-
-                }
-            Check_Amount();
-            sql.Close();
         }
 
         public void Check_Amount()
         {
+            string smtpServer = "smtp.mail.ru";
+            string from = "hedgehoggy@mail.ru";
+            string password = "31415926afaf";
+            string mailto = "";
+            string contractor = "";
 
-            pdf.Add_PDF(name, amount, measure, date);
+            string contractorid = "";
+            string check_mail1 = "SELECT ContractorID FROM Goods WHERE Name LIKE '" + Convert.ToString(cMBgoods.SelectedItem) + "'";
+            SQLiteCommand get_mail1 = new SQLiteCommand(check_mail1, sql);
+            SQLiteDataReader reader_mail1 = get_mail1.ExecuteReader();
+            while (reader_mail1.Read())
+            {
+                contractorid = reader_mail1[0] + "";
+            }
+
+
+            string check_mail = "SELECT Email FROM Contractor  WHERE ContractorID LIKE " + contractorid;
+            SQLiteCommand get_mail = new SQLiteCommand(check_mail, sql);
+            SQLiteDataReader reader_mail = get_mail.ExecuteReader();
+            while (reader_mail.Read())
+            {
+                mailto = reader_mail[0] + "";
+            }
+            string check_contractor = "SELECT ContractorName FROM Contractor WHERE Email LIKE '" + mailto + "'";
+            SQLiteCommand get_contractor = new SQLiteCommand(check_contractor, sql);
+            SQLiteDataReader reader_contractor = get_contractor.ExecuteReader();
+            while (reader_contractor.Read())
+            {
+                contractor = reader_contractor[0] + "";
+            }
+            string caption = "Заказ";
+            string message = "Уважаемый" + contractor + " напоминаем вам о том что необходимо поставить товар. Счёт прилагается к письму";
+            string attachFile = @"C:\Temp\PdfFile.pdf";
+            pdf.Add_PDF(id, name, amount, measure, date, price, final_price);
+            Mail.SendMail(smtpServer, from, password, mailto, caption, message, attachFile);
+        }
+
+
+        private void nUDmonth_ValueChanged_1(object sender, EventArgs e)
+        {
+            if (nUDmonth.Value == 2)
+            {
+                nUDday.Maximum = 28;
+            }
+            else
+                if ((nUDmonth.Value == 4) || (nUDmonth.Value == 6) || (nUDmonth.Value == 9) || (nUDmonth.Value == 11))
+                {
+                    nUDday.Maximum = 30;
+                }
+                else
+                    nUDday.Maximum = 31;
+
         }
 
     }
